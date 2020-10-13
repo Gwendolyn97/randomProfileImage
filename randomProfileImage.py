0022df6c3c9490de40e6975a4182317834c7dc10
Gwendoughlyn <70076973+Gwendolyn97@users.noreply.github.com>
2020-10-13 17:07:21 -0500

    Add files via upload

---
+++ randomProfileImage.py
@@ -0,0 +1,53 @@
+import os
+import random
+import schedule
+import time
+import tweepy
+
+from dotenv import load_dotenv
+load_dotenv()
+consumer_key = os.getenv("consumer_key")
+consumer_secret = os.getenv("consumer_secret")
+access_token = os.getenv("access_token")
+access_secret = os.getenv("access_secret")
+
+def twitter():  #returns api so that i dont have to copy and paste this in every fucking bit i want to use the twitter api 
+                #like holy fucking shit there has to be an easier way to do thi 78y6juhigtbn
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+
+    api = tweepy.API(auth)
+    return api
+
+directory = r"C:\\Users\\Gwendolyn\Desktop\Bots (Twitter)\\Random Profile Image\\images"
+
+print(f"{os.listdir(directory)}")
+
+
+def bot(): 
+    try: 
+        image = random.choice(os.listdir(f"{directory}"))
+        print(f"{image} has been selected.")
+
+        twitter().update_profile_image(filename = f"{directory}\\{image}")
+        print(f"Now using {image} as the profile picutre!")
+
+        os.remove(f"{directory}\\{image}")        
+        print(f"Task completed.\n-----")    
+    except tweepy.TweepError as e: 
+        print(f"Exception raised!\nCode: {e.api_code}\nReason: {e.reason}")
+    
+        if e.reason == "File is too big, must be less than 700kb.":
+            print(f"{image} is too large to use. Deleting!")
+            os.remove(f"{directory}\\{image}")
+            print(f"There are {len(os.listdir(directory))} images left in {directory}")
+    finally: 
+        pass
+
+schedule.every().day.at("00:00").do(bot)
+
+while len(os.listdir(f"{directory}")) != 0:
+    schedule.run_pending()
+    time.sleep(1)
+else:
+    print(f"{directory} is empty.")
